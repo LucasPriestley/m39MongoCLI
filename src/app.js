1fd15125eebb6d9397cdc8fc06f6f8f1691046af
Lucas <104462643+LucasPriestley@users.noreply.github.com>
2022-07-20 17:09:07 +0100

    search with title or actor

--- src/app.js
+++ src/app.js
@@ -12,18 +12,29 @@ const app = async (yargsObj) => {
     //add a movie from the terminal into an object and save in database
   } else if (yargsObj.read) {
     console.log("R");
-    const results = await collection.find({title: yargsObj.title, actor: yargsObj.actor}).toArray();
-    console.log(results);
+    const result = yargsObj.title
+      ? await collection.find({ title: yargsObj.title }).toArray()
+      : await collection.find({ actor: yargsObj.actor }).toArray();
+    if (result == "") {
+      console.log("Not found");
+    } else {
+      console.log(result);
+    }
+
     //list items from database
   } else if (yargsObj.update) {
     console.log("U");
-    const update = new UpdateMovie(yargsObj.title, yargsObj.newTitle, yargsObj.newActor);
+    const update = new UpdateMovie(
+      yargsObj.title,
+      yargsObj.newTitle,
+      yargsObj.newActor
+    );
     await update.updateMethod(collection);
     //update one database entry
   } else if (yargsObj.delete) {
     console.log("D");
-    const deletes = await collection.deleteOne({title: yargsObj.title});
-    console.log(deletes)
+    const deletes = await collection.deleteOne({ title: yargsObj.title });
+    console.log(deletes);
     // deletes.deleteOne(collection);
     //delete one database entry
   } else {
